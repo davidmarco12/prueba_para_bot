a69249a6c666334a2f4cfe6452f1b930463202a6
davidmarco12 <marcolindavid1@gmail.com>
2025-06-22 22:14:00 +0000

    prueba

--- util.py
+++ util.py
@@ -1,17 +1,5 @@
-def calcular_area_rectangulo(ancho, alto):
-    """Calcula el área de un rectángulo"""
+def calcular_area(ancho, alto):
     return ancho * alto
 
-def obtener_area_rectangulo(width, height):
-    """Obtiene el área de un rectángulo"""
-    resultado = width * height
-    return resultado
-
-def sumar_numeros(a, b):
-    """Suma dos números"""
-    return a + b
-
-def agregar_numeros(x, y):
-    """Agrega dos números"""
-    total = x + y
-    return total
+def obtener_area(width, height):
+    return width * height  
